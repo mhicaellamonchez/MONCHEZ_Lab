f04bad4df0a20e1aef8f11da62b791b0d349e6e6
mhicaellamonchez <148019849+mhicaellamonchez@users.noreply.github.com>
2023-10-16 08:45:51 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void OnVerticalStart(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Start;
+    }
+
+    private void OnVerticalCenter(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Center;
+    }
+
+    private void OnVerticalEnd(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.End;
+    }
+
+    private void OnHorizontalStart(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Start;
+    }
+
+    private void OnHorizontalCenter(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Center;
+    }
+
+    private void OnHorizontalEnd(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.End;
+    }
+
+
+}
+
